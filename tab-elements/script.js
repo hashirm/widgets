86ae208dd3e557a45e08bfb30558869f77c55410
Muhammad Hashir <37501188+hashirm@users.noreply.github.com>
2024-02-05 22:40:00 +0500

    widget code updated

--- tab-elements/script.js
+++ tab-elements/script.js
@@ -5,11 +5,17 @@ const tabsContent = document.querySelectorAll(".icon-img");
 tabs.forEach((tab) => {
   tab.addEventListener("click", (e) => {
     e.preventDefault();
+    let btnTab = e.target.getAttribute("data-tab");
+    console.log(btnTab);
     tabs.forEach((tab) => {
       tab.classList.remove("operationTab--active");
     });
     const clicked = e.target.classList.add("operationTab--active");
-    if(clicked.classList.)
-    
+    if (clicked) return;
+    tabsContent.forEach((element) =>
+      element.classList.remove("operation-content--active")
+    );
+    const textCont = document.querySelector(`.content-0${btnTab}`);
+    console.log(textCont.classList.add("operation-content--active"));
   });
 });
